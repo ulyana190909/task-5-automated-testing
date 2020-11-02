10cfdbaf2eba30dc2ab00207a44f99c9a466096b
Uliana Utina <ulyana190909@yandex.ru>
2020-11-02 13:11:27 +0300

    исправление теста

--- src/test/java/ru/netology/domain/СardDeliveryOrderTest.java
+++ src/test/java/ru/netology/domain/СardDeliveryOrderTest.java
@@ -6,24 +6,24 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class СardDeliveryOrderTest {
-   private RegistrationByCard registrationByCard;
-
+   private final String city = DataGenerator.getCity();
+   private final String date = DataGenerator.getDate();
+   private final String name = DataGenerator.getName();
+   private final String phone = DataGenerator.getPhone();
+   
     @Test
     void shouldSendValidRequest() {
         open("http://localhost:9999");
-        $("[data-test-id='city'] .input__control").setValue(registrationByCard.getCity());
+        $("[data-test-id='city'] .input__control").setValue(city);
         $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(registrationByCard.getDate());
-        $("[data-test-id='name'] .input__control").setValue(registrationByCard.getName());
-        $("[data-test-id='phone'] .input__control").setValue(registrationByCard.getPhoneNumber());
+        $("[data-test-id='date'] .input__control").setValue(date);
+        $("[data-test-id='name'] .input__control").setValue(name);
+        $("[data-test-id='phone'] .input__control").setValue(phone);
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Запланировать")).click();
         $(byText("Успешно!")).waitUntil(visible, 15000);
-        $("[data-test-id=success-notification] .notification__content").shouldHave(text("Встреча успешно запланирована на " + registrationByCard.getDate()));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(text("Встреча успешно запланирована на " + date));
     }
 
-//        LocalDate date = LocalDate.now().plusDays(3);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-//        String dateOfDelivery = formatter.format(date);
 }
 
